b3a1f9edd29b6512b28d656666b969e508c78c12
Sneha Jain <snehaanora@gmail.com>
2020-12-20 00:29:54 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Credit Management System</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="container intro">
+    <h1>THE SPARKS FOUNDATION </h1>
+   
+    <h2>BASIC BANKING SYSTEM</h2>
+    <h3>#TASK 1</h3>
+    <h1 class="name">Designed by:- Sneha Jain</h1>
+    <br>
+    <br><br>
+    <a href="./index1.php" class="btn">Start Transaction</a>
+</div>
+</html>
+</body>
---
+++ index1.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Credit Management System</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+ 
+  <div >
+
+    <section class="container heading-wrapper">
+      <h2>Welcome to</h2>
+      <h1>MFC BANK</h1>
+      <br>
+      <br>
+      <br>
+      <p>Transfer money within seconds!</p>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br
+      ><br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <a href="./all_users.php">View users</a> 
+    </section>
+  </div>
+  
+</body>
+</html>
